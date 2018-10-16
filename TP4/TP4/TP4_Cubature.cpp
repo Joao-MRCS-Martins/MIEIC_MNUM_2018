5409c4a84aa278947593f8d0afd4d8f2a1eed236
João Miguel Martins <up201707311@fe.up.pt>
2018-10-16 19:14:41 +0100

    Complete functions for cubature
    Trapeze and simpson methods

--- TP4/TP4/TP4_Cubature.cpp
+++ TP4/TP4/TP4_Cubature.cpp
@@ -7,25 +7,68 @@
 using namespace std;
 
 double funcIntDouble(double x, double y) {
-	return exp(y - x);
+	return exp(y-x);
 }
 
-//n=2 a=0 b=0.5 h=b-a/2 hx=hy=0.25
+double sumVertices(double a1,double b1,double a2,double b2) {
+	return funcIntDouble(a1, a2) + funcIntDouble(a1, b2) + funcIntDouble(b1, a2) + funcIntDouble(b1, b2);
+}
 
-double sumVertices() {
-	double sum = funcIntDouble(0, 0) + funcIntDouble(0, 0.5) + funcIntDouble(0.5, 0) + funcIntDouble(0.5, 0.5);
+double sumInterm(double a1, double hx, double a2, double hy, int n) {
+	double sum = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((i > 0 || i < (n - 1)) && (j > 0 || i < (n - 1)))
+				continue;
+			else
+				sum += funcIntDouble(a1 + i * hx, a2 + j * hy);
+		}
+	}
 	return sum;
 }
 
-double sumInterm() {
-	double sum = funcIntDouble(0, 0.25) + funcIntDouble(0.25, 0) + funcIntDouble(0.5, 0.25) + funcIntDouble(0.25, 0.5);
+double centralPoint(double a1, double hx, double a2, double hy, int n) {
+	double sum = 0;
+	for (int i = 1; i < (n - 1); i++) {
+		for (int j = 1; j < (n - 1); j++) {
+			sum += funcIntDouble(a1 + i * hx, a2 + j * hy);
+		}
+	}
 	return sum;
 }
 
-double centralPoint() {
-	return funcIntDouble(0.25, 0.25);
+double solTrapeze(double a1,double b1,double a2,double b2,int n) {
+	double hx = (b1 - a1) / n;
+	double hy = (b2 - a2) / n;
+	return hx*hy / 4 * (sumVertices(a1,b1,a2,b2) + 2 * sumInterm(a1, hx, a2, hy, n) + 4 * centralPoint(a1, hx, a2, hy, n));
 }
 
-double solTrapeze() {
-	return 0.25*0.25 / 4 * (sumVertices() + 2 * sumInterm() + 4 * centralPoint());
+double solSimpson(double a1, double b1, double a2, double b2, int n) {
+	double hx = (b1 - a1) / n;
+	double hy = (b2 - a2) / n;
+	return hx*hy / 9 * (sumVertices(a1, b1, a2, b2) + 4 * sumInterm(a1, hx, a2, hy, n) + 16 * centralPoint(a1, hx, a2, hy, n));
+}
+
+int main() {
+	int method;
+	cout << "The equation is: e^(y-x). Change the equation on the function to fit your needs." << endl << "Beware of values uninputtable on the console, like pi or euler's number." << endl;
+	cout << "Which numerical method you want to use to solve the double integral? " << endl << endl;
+	cout << "1 - Trapeze method." << endl
+		<< "2 - Simspson method." << endl;
+	cout << "-> ";
+	cin >> method;
+	
+	double a1, b1, a2, b2;
+	int n;
+	cout << endl << "Insert intervals of x, a1 & b1, and intervals of y, a2 & b2, and the number of intervals: " << endl;
+	cin >> a1 >> b1 >> a2 >> b2 >> n;
+	switch (method) {
+	case 1:
+		cout << "The solution is : " << solTrapeze(a1,b1,a2,b2,n) << endl;
+		break;
+	case 2:
+		cout << "The solution is : " << solSimpson(a1, b1, a2, b2, n) << endl;
+		break;
+	}
+	return 0;
 }
